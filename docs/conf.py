2016a70c76c7dd03d74630ca2bb5e673eb6ef988
Jeff Knupp <jknupp@appnexus.com>
2014-05-07 23:14:09 +0200

    fix docs

--- docs/conf.py
+++ docs/conf.py
@@ -28,6 +28,7 @@ import os
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.doctest',
     'sphinx.ext.coverage',
